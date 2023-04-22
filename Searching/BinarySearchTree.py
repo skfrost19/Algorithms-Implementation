263c95f8a32581a8e089e5854b12abbae2bd2592
skfrost19 <saahhiill123456@gmail.com>
2023-04-22 15:39:38 +0530

    made helper function anonymous

--- Searching/BinarySearchTree.py
+++ Searching/BinarySearchTree.py
@@ -16,27 +16,27 @@ class BinarySearchTree:
         return Node(val)
     
     def add(self, val):
-        self.head = self.add_helper(self.head, Node(val))
+        self.head = self.__add_helper(self.head, Node(val))
     
-    def add_helper(self, root, node):
+    def __add_helper(self, root, node):
         if root == None:
             return node
         if root.val > node.val:
-            root.left = self.add_helper(root.left, node)
+            root.left = self.__add_helper(root.left, node)
         else:
-            root.right = self.add_helper(root.right, node)
+            root.right = self.__add_helper(root.right, node)
         return root
     
     def print_tree(self):
         # inorder
-        self.print_tree_helper(self.head)
+        self.__print_tree_helper(self.head)
 
-    def print_tree_helper(self, head):
+    def __print_tree_helper(self, head):
         if head == None:
             return
-        self.print_tree_helper(head.left)
+        self.__print_tree_helper(head.left)
         print(head.val , " ")
-        self.print_tree_helper(head.right)
+        self.__print_tree_helper(head.right)
 
 class Test(unittest.TestCase):
     def test_add(self):
